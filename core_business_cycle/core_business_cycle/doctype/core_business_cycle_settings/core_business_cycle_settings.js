51803abd6316311b4fac3af00cf5ca256011418f
sibikumarkuppusamy <sibi.kumar@thirvusoft.in>
2022-06-24 11:31:39 +0530

    js changes

---
+++ core_business_cycle/core_business_cycle/doctype/core_business_cycle_settings/core_business_cycle_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, BC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Core Business Cycle Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
